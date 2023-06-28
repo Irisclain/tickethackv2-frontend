b127cb3a3a5d56c30c40da87be01374beeba027f
Benjamin Diomat <benjamin.diomat@gmail.com>
2023-06-28 17:26:44 +0200

    commit

--- cart.js
+++ cart.js
@@ -6,13 +6,13 @@ fetch(`http://localhost:3000/cart/trip`)
     console.log(data.newTrip[0].trip[0].departure)
     if(data.result) {
         document.querySelector('#changePart').remove()
-        for (let i = 0; i < data[0].trip.length; i++) {
+        for (let i = 0; i < data.newTrip.length; i++) {
             document.querySelector('#changePart').innerHTML += `<div>
             <div class="divider"></div>
           </div>
           <p class="message">No tickets in your cart.</p>
           <div class=trip>
-                    <p> ${data[0].trip[i].departure} > ${data.trip[i].arrival} </p>
+                    <p> ${data.newTrip[i].trip[i].departure} > ${data.trip[i].arrival} </p>
                     <p>${heure} </p>
                     <p>${data.trip[i].price}&#8364</p>
                     <button id="book">Book</button>
